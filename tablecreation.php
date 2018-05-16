023e06c4b230a15c3d50c6d5d134b4bd95bffdae
Dino Kurtagic <dkurtagic@gmail.com>
2018-05-16 21:08:01 +0200

    init commit

---
+++ tablecreation.php
@@ -0,0 +1,27 @@
+<?php
+ $host = "192.168.1.2";
+ $user = "root";
+ $pwd = "";
+ $db = "localdb";
+
+ //Database=localdb;Data Source=127.0.0.1:56044;User Id=azure;Password=6#vWHD_$
+ // Connect to database.
+ try {
+ $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+ $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+ }
+ catch(Exception $e){
+ die(var_dump($e));
+ }
+
+
+ $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+VARCHAR(30), date DATE);
+";
+
+ $stmt = $conn->prepare($sql_create);
+
+ $stmt->execute();
+
+ ?>
