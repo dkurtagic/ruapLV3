d03a3d584ca1c47df1267e08999c86c8a9bebf34
Dino Kurtagic <dkurtagic@gmail.com>
2018-05-16 22:31:08 +0200

    changed to external mysql

--- tablecreation.php
+++ tablecreation.php
@@ -3,6 +3,15 @@
     $user = "ruapUser";
     $pwd = "ruap1234";
     $db = "ruap";
+
+    $con = mysqli_connect($hosst,$user,$pwd);
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
 /*
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
