0ed4b50bc5294b5173dcae3ec21f6fec0c775e82
Dino Kurtagic <dkurtagic@gmail.com>
2018-05-16 23:13:47 +0200

    changed to external mysql

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date("Y-m-d");
 // Insert data
-$sql_insert = "INSERT INTO registration_tbl (name, email, date)
-VALUES ($name,$email,$date)";
+$sql_insert = "INSERT INTO registration_tbl ("name", "email", "date")
+VALUES (name,email,date)";
 if ($conn->query($sql_insert) === TRUE) {
   echo "<h3>Your're registered!</h3>";
 } else {
