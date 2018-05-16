0151abadb05ffa09a3ae626a2455664cdbe848f6
Dino Kurtagic <dkurtagic@gmail.com>
2018-05-16 22:32:57 +0200

    changed to external mysql

--- tablecreation.php
+++ tablecreation.php
@@ -11,39 +11,5 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-?>
-/*
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-
-    if(! $conn ) {
-      die('Could not connect: ' . mysqli_error());
-    }
-*/
-    echo 'Connected successfully';
-/*
-    $sql = 'CREATE Database ruap';
-    $retval = mysql_query( $sql, $conn );
-
-    if(! $retval ) {
-        die('Could not create database: ' . mysql_error());
-    }
-
-    echo "Database ruap created successfully\n";
-
-
-    $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
-    AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
-    VARCHAR(30), date DATE);";
-
-    mysql_select_db(db);
-    $retval = mysql_query( $sql, $conn );
-
-    if(! $retval ) {
-      die('Could not create table: ' . mysql_error());
-    }
-
-    echo "Table registration created successfully\n";
-
-    mysqli_close($conn);
-    */
+  
 ?>
