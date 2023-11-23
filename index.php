1773f5e6f2bc087816e88da2d6aa36c4a700cb08
dkurtagic <dkurtagic@gmail.com>
2023-11-24 00:21:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ Name <input type="text" name="name" id="name"/></br> Email <input type="text" na
 <?php
 // DB connection info
 //TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved earlier from the Azure Portal. $host = "value of Data Source";
-$host = "ruapdbserver.mysql.database.azure.com";
-$user = "ruapUser@ruapdbserver";
-$pwd = "Ruap1234";
-$db = "ruap";
+$host = "ruaplv3database-server.mysql.database.azure.com";
+$user = "jbxtqhjyvm";
+$pwd = "15YIEYB3SO8LV54B";
+$db = "ruaplv3database-server";
 // Connect to database.
 
 $conn = mysqli_connect($host,$user,$pwd,$db);
